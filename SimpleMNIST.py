5ad19275f2bf3bfa9475d715a3d18cf4c04d52d9
VampiroJudo <johnconley507@hotmail.com>
2018-11-11 16:44:14 -0500

    Building out initializer, session and test accuracy code to test the accuracy of our digit recognizer

--- SimpleMNIST.py
+++ SimpleMNIST.py
@@ -29,3 +29,30 @@ cross_entropy = tf.reduce_mean(
 
 # each training step in gradient decent we want to minimize cross_entropy
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# initialize the global variables
+init = tf.global_variables_initializer()
+
+# create an itneractive session that can span multiple code blacks. Don't
+#forget to explicitly close the session with sess.close()
+sess = tf.Session()
+
+# perform the Initialization which is only the Initialization of all global variables
+sess.run(init)
+
+# Perform 1000 training steps
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)    # get 100 random data pints from the data. batch xs = image,
+                                                        # batch ys = digit(0-9) class
+
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys}) # do the Optimization with this data
+
+# Evaluate how well the model did. Do this by comparing the digit with the highest probability in
+# actual (y) and predicted (_y)
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+test_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+print("Test accuracy: {0}%".format(test_accuracy * 100.0))
+
+sess.close()
